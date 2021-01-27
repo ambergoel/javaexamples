ff466d74091ebfac1bd24fefcc4923d787c6ae7d
Amber Goel <A48723@uniper.energy>
2021-01-27 14:46:41 +0100

    introduce @FunctionalInterface

--- src/io/agoel/IGreeting.java
+++ src/io/agoel/IGreeting.java
@@ -1,5 +1,11 @@
 package io.agoel;
 
+/*
+ * Marking with @FunctionalInterface which is make sure developer can not add more then one method 
+ * declaration here in interface. As this interface is created for Lambda expression.
+ * This annotation is completely optional but good to have.
+ */
+@FunctionalInterface
 public interface IGreeting {
 
 	void perform();
